1ebf3e0f1de0b137d5bd579bd13e35439ca3b370
Shorya Sethia <132898518+shoryasethia@users.noreply.github.com>
2025-06-16 01:01:32 +0530

    Added project on keyboard event

--- projects/5-keyboard/chaiaurcode.js
+++ projects/5-keyboard/chaiaurcode.js
@@ -1,1 +1,20 @@
-console.log('Project 5');
+const insert = document.getElementById('insert');
+
+window.addEventListener('keydown', function (e) {
+  insert.innerHTML = `
+    <div class='color'>
+      <table>
+        <tr>
+          <th>Key</th>
+          <th>Key Code</th>
+          <th>Code</th>
+        </tr>
+        <tr>
+          <td>${e.key === ' ' ? 'Space' : e.key}</td>
+          <td>${e.keyCode}</td>a
+          <td>${e.code}</td>
+        </tr>
+      </table>
+    </div>
+  `;
+});
