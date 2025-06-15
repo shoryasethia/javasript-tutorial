f150aeda6211f2f8c13766577ab0777873cd92da
Shorya Sethia <132898518+shoryasethia@users.noreply.github.com>
2025-06-16 01:18:50 +0530

    Fix href from home button in projects

--- projects/5-keyboard/chaiaurcode.js
+++ projects/5-keyboard/chaiaurcode.js
@@ -10,8 +10,8 @@ window.addEventListener('keydown', function (e) {
           <th>Code</th>
         </tr>
         <tr>
-          <td>${e.key === ' ' ? 'Space' : e.key}</td>
-          <td>${e.keyCode}</td>a
+          <td>${(e.key === ' ') ? 'Space' : e.key}</td>
+          <td>${e.keyCode}</td>
           <td>${e.code}</td>
         </tr>
       </table>
